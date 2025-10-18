424adee3b5d27fe61f317fb9a53426bb99d50cfa
Showrya Shetty <shakthishetty154@gmail.com>
2025-10-19 01:52:40 +0530

    serveless

--- api/index.ts
+++ api/index.ts
@@ -184,10 +184,10 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   const path = req.path;
   let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
-  const originalResJson = res.json;
-  res.json = function (bodyJson: any, ...args: any[]) {
+const originalResJson = res.json.bind(res) as (body?: any) => Response;
+  res.json = function (bodyJson?: any) {
     capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
+    return originalResJson(bodyJson);
   };
 
   res.on("finish", () => {
